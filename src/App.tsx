32f7fd4562a0568d2e7474069c6ea5dd77ba9af4
Moises Alvarez <mfalvare@espol.edu.ec>
2024-07-08 11:21:27 -0500

    actualizacion de guia 13

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,46 @@
+import "./App.css";
 
-import './App.css'
-
-import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
-import Indicator from './components/Indicator';
-import Summary from './components/Summary';
-import BasicTable from './components/BasicTable';
-
+import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
+import Indicator from "./components/Indicator";
+import Summary from "./components/Summary";
+import BasicTable from "./components/BasicTable";
+import WeatherChart from "./components/WeatherChart";
+import ControlPanel from "./components/ControlPanel";
 function App() {
-
-
   return (
     <Grid container spacing={5}>
-      <Grid xs={12}  lg={2}>
-	        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
-	    </Grid>
-      <Grid xs={12}  lg={2}>
-	        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
-	    </Grid> 
-      <Grid xs={12}  lg={2}>
-	        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
-	    </Grid>
-      <Grid xs={12}  lg={2}>
-	        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
-	    </Grid>   
-      <Grid xs={12}  lg={2}>
-	        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
-	    </Grid>
-      <Grid xs={12}  lg={2}>
-	        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
-	    </Grid>         
-      <Grid xs={12}  lg ={2}>
-	        <Summary></Summary>
-	    </Grid>
-      <Grid xs={12} md={6} lg={10} >
-	       <BasicTable />
-	    </Grid> 
-    
+      <Grid xs={12} lg={2}>
+        <Indicator title="Precipitación" subtitle="Probabilidad" value={0.13} />
+      </Grid>
+      <Grid xs={12} lg={2}>
+        <Indicator title="Precipitación" subtitle="Probabilidad" value={0.13} />
+      </Grid>
+      <Grid xs={12} lg={2}>
+        <Indicator title="Precipitación" subtitle="Probabilidad" value={0.13} />
+      </Grid>
+      <Grid xs={12} lg={2}>
+        <Indicator title="Precipitación" subtitle="Probabilidad" value={0.13} />
+      </Grid>
+      <Grid xs={12} lg={2}>
+        <Indicator title="Precipitación" subtitle="Probabilidad" value={0.13} />
+      </Grid>
+      <Grid xs={12} lg={2}>
+        <Indicator title="Precipitación" subtitle="Probabilidad" value={0.13} />
+      </Grid>
+      <Grid xs={12} lg={2}>
+        <Summary></Summary>
+      </Grid>
+      <Grid xs={12} md={6} lg={10}>
+        <BasicTable />
+      </Grid>
+      <Grid xs={12} lg={2}>
+        <ControlPanel />
+      </Grid>
+      <Grid xs={12} md={12} lg={12}>
+        <WeatherChart></WeatherChart>
+      </Grid>
     </Grid>
-    )
+  );
 }
 
-export default App
+export default App;
